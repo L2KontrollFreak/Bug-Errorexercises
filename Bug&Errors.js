d60a3522570d49338e544b2d6353cf6e1d8e7b30
L2KontrollFreak <damarble2005@gmail.com>
2022-03-22 19:19:55 -0700

    finish 2 js codes

--- Bug&Errors.js
+++ Bug&Errors.js
@@ -1,3 +1,21 @@
-class MultipyUnitFail extends console.error{}
+class MultipyUnitFail extends Error {}
 
-function primitiveMutiply(a,b)
+function primitiveMutiply(a, b) {
+    if (Math.random() < 0.2) {
+        return a * b;
+    } else {
+        throw new MultipyUnitFail("Bump");
+    }
+}
+    function multiplyTrue(a, b) {
+        for (; ;) {
+            try {
+                return trueMultiply(a, b);
+            } catch (e) {
+                if (!(e instanceof MultipyUnitFail))
+                    throw e;
+            }
+        }
+    }
+
+console.log(multiplyTrue(7,7));
---
+++ Bug&Errors2.js
@@ -0,0 +1,38 @@
+const contain ={
+    locked: true,
+    unlock() {this.locked = false;},
+    Lock() {this.locked = true;},
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("locked!");
+        return this._content;
+    }
+};
+
+function withContainUnlocked(body){
+    let locked = contain.locked;
+    if (!locked){
+        return body();
+    }
+
+    contain.unlock();
+    try {
+        return body();
+    } finally{
+        contain.Lock();
+    }
+    }
+
+    withContainUnlocked(function(){
+        contain.content.push("Money");
+    });
+
+    try{
+        withContainUnlocked(function() {
+            throw new Error("it's the FBI! Abort the Mission and Run!!!");
+        });
+        } catch (e) {
+            console.log("Error raised:",e);
+        }
+
+        console.log(contain.locked);
